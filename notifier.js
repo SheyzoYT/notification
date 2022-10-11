d36ba2c175a6456aed3d350e1f0df24ebd289979
SheyzoYT <88588553+SheyzoYT@users.noreply.github.com>
2022-10-11 12:46:10 +0200

    Update notifier.js

--- notifier.js
+++ notifier.js
@@ -1,6 +1,6 @@
 function test() {
     const TITLE = 'Eronix'
-    const BODY = 'Patch-update [1.0.0] 25/09/2022'
+    const BODY = 'Patch-update [1.0.0] 25/09/2029'
     const notification = new Notification(TITLE, {
         body: BODY,
         icon: "https://media.discordapp.net/attachments/825428957716807732/1026063883787579402/eronix.png?width=676&height=676"
