db9c3cfc439899029e30d3381c25f4bda1c6e645
SheyzoYT <88588553+SheyzoYT@users.noreply.github.com>
2022-10-11 12:51:37 +0200

    Create second.js

---
+++ second.js
@@ -0,0 +1,9 @@
+function test() {
+    const TITLE = 'Eronix'
+    const BODY = 'Alerte launcher'
+    const notification = new Notification(TITLE, {
+        body: BODY,
+        icon: "https://media.discordapp.net/attachments/825428957716807732/1026063883787579402/eronix.png?width=676&height=676"
+    })
+
+}
